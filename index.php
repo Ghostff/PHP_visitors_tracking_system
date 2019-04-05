566ca4f0adc2bfea573143d8d22e76cbc2604325
Thomas Rohlik <rohlik@3server.cz>
2019-04-05 23:54:49 +0200

    Fix typo in IP
    Fix typo in IP

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ include 'src/Stalk.php';
 # bad IP
 $stalk = new Stalk(function ($error) {
     var_dump($error);
-}, '88.163.1sd83.16');
+}, '88.163.183.16');
 
 
 $stalk = new Stalk(function ($error) {
